5e3d47c7aaf0a9d09b52a7fd1b7fdd261f1fe842
BEWW_S <76526978+Bewtysa@users.noreply.github.com>
2022-07-24 16:28:54 +0700

    hi

---
+++ T/ ToDoList/ndexdom.js
@@ -0,0 +1,55 @@
+// Create close button
+var ToDoList = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < ToDoList.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  ToDoList[i].appendChild(span);
+}
+
+// Close item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Checked
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// addBtn
+function addToDoList() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("ToDoText").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("ToDoList").appendChild(li);
+  }
+  document.getElementById("ToDoText").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
